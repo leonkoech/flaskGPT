3b3ea3ffe90d0b42fba65bcaf70c64f7544b7673
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2023-03-25 16:30:20 -0400

    add Chatbot function

--- api/Chatbot_for_VR.py
+++ api/Chatbot_for_VR.py
@@ -7,16 +7,15 @@ from dotenv import load_dotenv
 load_dotenv('.env.txt')
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-print(OPENAI_API_KEY)
 
 
-conversation = ConversationChain(
-memory= ConversationBufferMemory(),
-verbose=True,
-llm=OpenAI(temperature=0.9)
-)
+def chatGPT(input):
+  conversation = ConversationChain(
+  memory= ConversationBufferMemory(),
+  verbose=True,
+  llm=OpenAI(temperature=0.9)
+  )
 
-# output1 = conversation.predict(input = input("input here!"))
-output2 = conversation.predict(input = input("input here!"))
-
-print(output2)
+  # output1 = conversation.predict(input = input("input here!"))
+  output2 = conversation.predict(input)
+  return output2
